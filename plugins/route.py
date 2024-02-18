74a92c41b56c451fbc73329eb65505b8757efdd3
Moviez Bot Dev <143684485+Moviez-Bot-Dev@users.noreply.github.com>
2024-02-18 22:41:17 +0530

    Add files via upload

---
+++ plugins/route.py
@@ -0,0 +1,21 @@
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response(
+        {
+            "server_status": "running",
+            "uptime": get_readable_time(time.time() - StartTime),
+            "telegram_bot": "@" + MoviezMasalaBot.username,
+            "connected_bots": len(multi_clients),
+            "loads": dict(
+                ("bot" + str(c + 1), l)
+                for c, (_, l) in enumerate(
+                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
+                )
+            ),
+            "version": __version__,
+        }
+    )
